5d79e5084edfb182b5e641604561cfee83278de5
Suzukiti-1029 <h40396suzujun@gmail.com>
2023-01-21 09:37:36 +0000

    設定をsqlalchemy（mysql使用）用に変更

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,16 @@ import secrets
 
 def create_app(test_config=None):
   app = Flask(__name__, instance_relative_config=True)
-  app.config.from_mapping(
-    SECRET_KEY= '256133', #TODO デプロイ時secrets.token_hex()に変更
-    DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+  app.config.from_mapping( # TODO デプロイ：設定ファイルを外部に分離
+    SECRET_KEY='256133',
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
+      'user': 'suzukiti',
+      'password': '256133',
+      'host': 'mysql',
+      'db_name': 'dev_amtapp'
+    }),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    SQLALCHEMY_ECHO=True,
   )
 
   if test_config is None:
