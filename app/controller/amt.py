90196226e256e94204b06da560627d16622465b2
Suzukiti-1029 <h40396suzujun@gmail.com>
2023-01-24 04:27:51 +0000

    収支記録一覧のコントローラ作成

--- app/controller/amt.py
+++ app/controller/amt.py
@@ -1,7 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from app.database import db
+from app.models import Amt_fluctuations
 
 bp = Blueprint('amt', __name__, url_prefix='/amt')
 
 @bp.route('/')
 def index():
-  return 'amtのindex'
+  amt_flucs = Amt_fluctuations.query.all()
+  return render_template('amt/index.html', amt_flucs=amt_flucs)
